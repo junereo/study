67fb2caf33a002d1642f7b7cd862b360ff84b17e
junereo <taejung.joe.kim@gmail.com>
2024-12-18 17:35:38 +0900

    feat: 버튼 클릭시 메시지 표시 기능 추가

--- public/script.js
+++ public/script.js
@@ -3,12 +3,18 @@ const body = document.querySelector("body");
 const create = () =>{
     const headerDiv = document.createElement("div");
     const headerDivText = document.createElement("div");
+    const button = document.createElement("button");
 
     headerDiv.setAttribute("class","header");
     headerDivText.setAttribute("class","header-text");
+    button.setAttribute("class", "btn");
+    button.textContent = "버튼";
+    button.style.width = "100px";
+    button.style.height = "30px";
 
     body.append(headerDiv);
     headerDiv.append(headerDivText);
+    headerDiv.append(button);
 
 
     const header = document.querySelector(".header");
@@ -18,8 +24,14 @@ const create = () =>{
     headerText.style.color = "red";
 }
 
+const alram = (e) =>{
+    if(e.target.className === "btn"){
+        alert("환영 합니다.")
+    }
+}
 
 
+body.addEventListener("click", alram);
 console.log("환영 합니다");
 
 create();
