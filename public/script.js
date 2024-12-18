c1a62600937f40ef9e61ac49c77f9be024798717
junereo <taejung.joe.kim@gmail.com>
2024-12-18 17:23:27 +0900

    feat: DOM 조작 추가

--- public/script.js
+++ public/script.js
@@ -1,1 +1,24 @@
+const body = document.querySelector("body");
+
+const create = () =>{
+    const headerDiv = document.createElement("div");
+    const headerDivText = document.createElement("div");
+
+    headerDiv.setAttribute("class","header");
+    headerDivText.setAttribute("class","header-text");
+
+    body.append(headerDiv);
+    headerDiv.append(headerDivText);
+
+
+    const header = document.querySelector(".header");
+    const headerText = document.querySelector(".header-text");
+
+    headerText.textContent = "재미 있는 git 시간"
+}
+
+
+
 console.log("환영 합니다");
+
+create();
